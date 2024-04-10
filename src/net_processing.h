dergoegge/bitcoin

[net processing] Move nTimeOffset to net_processing

src/net_processing.h
@@ -39,6 +39,7 @@ struct PeerStats {
     std::string m_clean_subversion;
     bool m_bip152_highbandwidth_to{false};
     bool m_bip152_highbandwidth_from{false};
+    int64_t m_time_offset{0};
 };
 
 class PeerManager : public CValidationInterface, public NetEventsInterface

src/qt/rpcconsole.cpp
@@ -1171,7 +1171,6 @@ void RPCConsole::updateDetailWidget()
     ui->peerBytesRecv->setText(GUIUtil::formatBytes(stats->nodeStats.nRecvBytes));
     ui->peerPingTime->setText(GUIUtil::formatPingTime(stats->nodeStats.m_last_ping_time));
     ui->peerMinPing->setText(GUIUtil::formatPingTime(stats->nodeStats.m_min_ping_time));
-    ui->timeoffset->setText(GUIUtil::formatTimeOffset(stats->nodeStats.nTimeOffset));
     if (stats->nodeStats.nVersion) {
         ui->peerVersion->setText(QString::number(stats->nodeStats.nVersion));
     }
@@ -1191,6 +1190,7 @@ void RPCConsole::updateDetailWidget()
     // This check fails for example if the lock was busy and
     // m_peer_stats couldn't be fetched.
     if (stats->m_peer_stats_available) {
+        ui->timeoffset->setText(GUIUtil::formatTimeOffset(stats->m_peer_stats.m_time_offset));
         QString bip152_hb_settings;
         if (stats->m_peer_stats.m_bip152_highbandwidth_to) bip152_hb_settings = ts.to;
         if (stats->m_peer_stats.m_bip152_highbandwidth_from) bip152_hb_settings += (bip152_hb_settings.isEmpty() ? ts.from : QLatin1Char('/') + ts.from);

src/rpc/net.cpp
@@ -216,7 +216,7 @@ static RPCHelpMan getpeerinfo()
         obj.pushKV("bytessent", stats.m_send_bytes);
         obj.pushKV("bytesrecv", stats.nRecvBytes);
         obj.pushKV("conntime", count_seconds(stats.m_connected));
-        obj.pushKV("timeoffset", stats.nTimeOffset);
+        obj.pushKV("timeoffset", peer_stats.m_time_offset);
         if (stats.m_last_ping_time > 0us) {
             obj.pushKV("pingtime", Ticks<SecondsDouble>(stats.m_last_ping_time));
         }

shitcoinsherpa/Freycoin

src/net_processing.h
@@ -41,6 +41,7 @@ struct CNodeStateStats {
     bool m_addr_relay_enabled{false};
     ServiceFlags their_services;
     int64_t presync_height{-1};
+    int64_t time_offset{0};
 };
 
 class PeerManager : public CValidationInterface, public NetEventsInterface

src/qt/rpcconsole.cpp
@@ -1196,7 +1196,6 @@ void RPCConsole::updateDetailWidget()
     ui->peerBytesRecv->setText(GUIUtil::formatBytes(stats->nodeStats.nRecvBytes));
     ui->peerPingTime->setText(GUIUtil::formatPingTime(stats->nodeStats.m_last_ping_time));
     ui->peerMinPing->setText(GUIUtil::formatPingTime(stats->nodeStats.m_min_ping_time));
-    ui->timeoffset->setText(GUIUtil::formatTimeOffset(stats->nodeStats.nTimeOffset));
     if (stats->nodeStats.nVersion) {
         ui->peerVersion->setText(QString::number(stats->nodeStats.nVersion));
     }
@@ -1228,6 +1227,7 @@ void RPCConsole::updateDetailWidget()
     // This check fails for example if the lock was busy and
     // nodeStateStats couldn't be fetched.
     if (stats->fNodeStateStatsAvailable) {
+        ui->timeoffset->setText(GUIUtil::formatTimeOffset(stats->nodeStateStats.time_offset));
         ui->peerServices->setText(GUIUtil::formatServicesStr(stats->nodeStateStats.their_services));
         // Sync height is init to -1
         if (stats->nodeStateStats.nSyncHeight > -1) {

src/rpc/net.cpp
@@ -239,7 +239,7 @@ static RPCHelpMan getpeerinfo()
         obj.pushKV("bytessent", stats.nSendBytes);
         obj.pushKV("bytesrecv", stats.nRecvBytes);
         obj.pushKV("conntime", count_seconds(stats.m_connected));
-        obj.pushKV("timeoffset", stats.nTimeOffset);
+        obj.pushKV("timeoffset", statestats.time_offset);
         if (stats.m_last_ping_time > 0us) {
             obj.pushKV("pingtime", Ticks<SecondsDouble>(stats.m_last_ping_time));
         }