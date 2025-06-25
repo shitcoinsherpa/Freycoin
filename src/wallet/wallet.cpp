bitcoin/bitcoin

wallet: Recalculate the wallet's txos after any imports

src/wallet/wallet.cpp
@@ -4493,4 +4493,12 @@ void CWallet::RefreshWalletTxTXOs(const CWalletTx& wtx)
         }
     }
 }
+
+void CWallet::RefreshAllTXOs()
+{
+    AssertLockHeld(cs_wallet);
+    for (const auto& [_, wtx] : mapWallet) {
+        RefreshWalletTxTXOs(wtx);
+    }
+}
 } // namespace wallet

shitcoinsherpa/Freycoin

src/wallet/wallet.cpp
@@ -4451,4 +4451,12 @@ void CWallet::RefreshTXOsFromTx(const CWalletTx& wtx)
         }
     }
 }
+
+void CWallet::RefreshAllTXOs()
+{
+    AssertLockHeld(cs_wallet);
+    for (const auto& [_, wtx] : mapWallet) {
+        RefreshTXOsFromTx(wtx);
+    }
+}
 } // namespace wallet